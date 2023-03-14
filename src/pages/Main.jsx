755286b481810ad09edd62772bceba159615a564
Yoshi Odei <yoshiodei@yahoo.com>
2023-03-14 16:28:38 -0700

    add all sections

--- src/pages/Main.jsx
+++ src/pages/Main.jsx
@@ -1,8 +1,20 @@
 import React from 'react';
 import Home from '../sections/Home';
+import About from '../sections/About';
+import Scripture from '../sections/Scripture';
+import ServiceTimes from '../sections/ServiceTimes';
+import Join from '../sections/Join';
+import Sermons from '../sections/Sermons';
 
 export default function Main() {
   return (
-    <Home />
+    <>
+      <Home />
+      <About />
+      <Scripture />
+      <ServiceTimes />
+      <Join />
+      <Sermons />
+    </>
   );
 }
